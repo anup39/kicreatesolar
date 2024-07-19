9e532952b4b6ba4719364df239ca7d62c9d155f6
Anup Dahal <adahal39@gmail.com>
2024-07-19 23:40:28 +0545

    feat : slice for  user

--- src/store/slices/user.js
+++ src/store/slices/user.js
@@ -1,1 +1,63 @@
 import { createSlice } from "@reduxjs/toolkit";
+
+import axios from "../../utils/axios";
+import { dispatch } from "../index";
+
+const initialState = {
+  error: null,
+  usersS1: [],
+  usersS2: [],
+  followers: [],
+  friendRequests: [],
+  friends: [],
+  gallery: [],
+  posts: [],
+  detailsCards: [],
+  simpleCards: [],
+  profileCards: [],
+};
+
+const slice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    // HAS ERROR
+    hasError(state, action) {
+      state.error = action.payload;
+    },
+
+    // GET USERS STYLE 1
+    getUsersListStyle1Success(state, action) {
+      state.usersS1 = action.payload;
+    },
+
+    // GET USERS STYLE 2
+    getUsersListStyle2Success(state, action) {
+      state.usersS2 = action.payload;
+    },
+  },
+});
+
+export default slice.reducer;
+
+export function getUsersListStyle1() {
+  return async () => {
+    try {
+      const response = await axios.get("/api/user-list/s1/list");
+      dispatch(slice.actions.getUsersListStyle1Success(response.data.users_s1));
+    } catch (error) {
+      dispatch(slice.actions.hasError(error));
+    }
+  };
+}
+
+export function getUsersListStyle2() {
+  return async () => {
+    try {
+      const response = await axios.get("/api/user-list/s2/list");
+      dispatch(slice.actions.getUsersListStyle2Success(response.data.users_s2));
+    } catch (error) {
+      dispatch(slice.actions.hasError(error));
+    }
+  };
+}
