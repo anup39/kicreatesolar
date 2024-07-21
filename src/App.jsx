80ac9ca1cdc11826f3776e74e4fc33b67d6eb205
Anup Dahal <adahal39@gmail.com>
2024-07-21 08:22:21 +0545

    Added snackbar

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,8 @@
 import Locales from "./ui-component/Locales";
 import NavigationScroll from "./layout/NavigationScroll";
 import RTLLayout from "./ui-component/RTLLayout";
-// import Snackbar from "ui-component/extended/Snackbar";
-// import Notistack from "ui-component/third-party/Notistack";
+import Snackbar from "./ui-component/extended/Snackbar";
+import Notistack from "./ui-component/third-party/Notistack";
 
 // import ThemeCustomization from "themes";
 
