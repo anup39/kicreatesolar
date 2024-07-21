3ebfcae7b97b8d60566d8bc62fdff339d65fe958
Anup Dahal <adahal39@gmail.com>
2024-07-21 13:38:46 +0545

    fixed layout

--- src/layout/MainLayout/MenuList/index.jsx
+++ src/layout/MainLayout/MenuList/index.jsx
@@ -14,7 +14,7 @@ import menuItem from "../../../menu-items";
 import useConfig from "../../../hooks/useConfig";
 
 import { MenuOrientation } from "../../../config";
-import { Menu } from "../../../menu-items/widget";
+// import { Menu } from "../../../menu-items/widget";
 import { HORIZONTAL_MAX_ITEM } from "../../../config";
 import { useGetMenu, useGetMenuMaster } from "../../../api/menu";
 
@@ -33,24 +33,11 @@ const MenuList = () => {
   const [selectedID, setSelectedID] = useState("");
   const [menuItems, setMenuItems] = useState({ items: [] });
 
-  let widgetMenu = Menu();
+  // let widgetMenu = Menu();
 
   useLayoutEffect(() => {
-    const isFound = menuItem.items.some((element) => {
-      if (element.id === "group-widget") {
-        return true;
-      }
-      return false;
-    });
-    if (menuLoading) {
-      menuItem.items.splice(1, 0, widgetMenu);
-      setMenuItems({ items: [...menuItem.items] });
-    } else if (!menuLoading && widgetMenu?.id !== undefined && !isFound) {
-      menuItem.items.splice(1, 1, widgetMenu);
-      setMenuItems({ items: [...menuItem.items] });
-    } else {
-      setMenuItems({ items: [...menuItem.items] });
-    }
+    setMenuItems({ items: [...menuItem.items] });
+
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [menuLoading]);
 
